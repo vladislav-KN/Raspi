6c00619fb35832695377df482a0b4d27f69414df
EZ <vnkolevatov@gmail.com>
2022-11-16 12:55:08 +0500

    nothing happened

--- client.py
+++
@@ -1,3 +0,0 @@
-import requests
-r = requests.get("http://192.168.0.15/VendingSys_Server/Order/Scan/1/1")
-print(r.text)
--- update.py
+++
@@ -1,3 +0,0 @@
-import git
-repo = git.Repo("<the file directory of your local gihub repo>")
-repo.remotes.origin.pull()
